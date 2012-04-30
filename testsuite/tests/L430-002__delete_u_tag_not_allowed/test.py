eaea7a365016603bcac9deb0a458b811882ded67
Joel Brobecker <brobecker@adacore.com>
2012-04-29 18:26:13 -0700

    L430-002__delete_u_tag_not_allowed: New testcase.

---
+++ testsuite/tests/L430-002__delete_u_tag_not_allowed/test.py
@@ -0,0 +1,18 @@
+from support import *
+import re
+
+class TestRun(TestCase):
+    def test_delete_unannotated_tag(self):
+        """Try deleting an unnanotated tag.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push origin :some-tag'.split())
+        self.assertNotEqual(p.status, 0, ex_run_image(p))
+        self.assertTrue(re.match('.*Deleting a tag is not allowed in '
+                                    'this repository',
+                                 p.out, re.DOTALL),
+                        ex_run_image(p))
+
+if __name__ == '__main__':
+    runtests()

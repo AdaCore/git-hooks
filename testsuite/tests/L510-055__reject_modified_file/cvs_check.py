73cdea7e0233abba6219613f5d25e64b73c2e0c2
Joel Brobecker <brobecker@adacore.com>
2012-05-26 11:52:08 +0200

    L510-055__reject_modified_file: New testcaes

---
+++ testsuite/tests/L510-055__reject_modified_file/cvs_check.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+"""A dummy cvs_check that pretends that certain files fail the style check.
+
+It also prints a trace on stdout, in order to allow us to allow us
+to verify that the script was called with the correct arguments.
+"""
+import sys
+
+# To help with testing, print a trace containing the name of the file
+# that is being checked.
+print >> sys.stderr, "cvs_check: `%s'" % sys.argv[1]
+
+# Fail the style-check for the following files:
+if sys.argv[1] == 'trunk/repo/b':
+    print >> sys.stderr, 'ERROR: style-check error detected.'
+    print >> sys.stderr, 'ERROR: Copyright year in header is not up to date'
+    sys.exit(1)
+
---
+++ testsuite/tests/L510-055__reject_modified_file/test.py
@@ -0,0 +1,30 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing multi-file commit on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push origin master'.split())
+
+        # The push should fail, because the pre-commit checker will
+        # refuse one of the updates.
+        self.assertTrue(p.status != 0, ex_run_image(p))
+
+        expected_out = (
+            r".*cvs_check: `trunk/repo/a'" +
+            r".*pre-commit check failed for file `b' at commit: " +
+                "4f0f08f46daf6f5455cf90cdc427443fe3b32fa3" +
+            r".*cvs_check: `trunk/repo/b'" +
+            r".*ERROR: style-check error detected\." +
+            r".*ERROR: Copyright year in header is not up to date" +
+            r".*error: hook declined to update refs/heads/master" +
+            r".*! \[remote rejected\] master -> master \(hook declined\)" +
+            r".*error: failed to push some refs to '\.\./bare/repo.git'")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+if __name__ == '__main__':
+    runtests()

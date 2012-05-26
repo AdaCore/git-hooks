82ba63a5bd55de2e75711096bb4213a0778bb73a
Joel Brobecker <brobecker@adacore.com>
2012-05-26 11:52:18 +0200

    L512-002__forced_update_precommit_err: New testcaes

---
+++ testsuite/tests/L512-002__forced_update_precommit_err/test.py
@@ -0,0 +1,28 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try non-fast-forward push on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push -f origin topic/new-feature'.split())
+
+        self.assertTrue(p.status != 0, ex_run_image(p))
+
+        expected_out = (
+            r".*WARNING: This is \*NOT\* a fast-forward update." +
+            r".*cvs_check: `trunk/repo/a'" +
+            r".*pre-commit check failed for file `b'" +
+            r".*cvs_check: `trunk/repo/b'" +
+            r".*ERROR: style-check error detected" +
+            r".*error: hook declined to update refs/heads/topic/new-feature" +
+            r".*\[remote rejected\]\s+" +
+            r"topic/new-feature\s+->\s+topic/new-feature\s+" +
+                r"\(hook declined\)")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+if __name__ == '__main__':
+    runtests()

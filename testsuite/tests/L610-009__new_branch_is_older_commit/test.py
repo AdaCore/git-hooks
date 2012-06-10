2c98e55fd638bc5496ccb34a8d0d3ff30fa18904
Joel Brobecker <brobecker@adacore.com>
2012-06-10 16:12:16 -0700

    Add L610-009__new_branch_is_older_commit.

---
+++ testsuite/tests/L610-009__new_branch_is_older_commit/test.py
@@ -0,0 +1,37 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing new branch on remote.
+
+        In this testcase, the release-0.1-branch points to a commit
+        that's one of the older commits in the "master" branch.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push origin release-0.1-branch'.split())
+
+        self.assertTrue(p.status == 0, ex_run_image(p))
+
+        expected_out = (
+            r".*\[new branch\]\s+release-0\.1-branch -> release-0\.1-branch")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+        # Verify that the branch has been created in the remote
+        # repository and that it points to the expected commit.
+
+        cd('%s/bare/repo.git' % TEST_DIR)
+
+        p = Run('git show-ref -s release-0.1-branch'.split())
+
+        self.assertTrue(p.status == 0, ex_run_image(p))
+
+        expected_out = (r"4a325b31f594b1dc2c66ac15c4b6b68702bd0cdf")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+if __name__ == '__main__':
+    runtests()

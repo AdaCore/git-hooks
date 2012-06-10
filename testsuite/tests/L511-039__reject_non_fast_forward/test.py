a76130be2b220525a7784d66967e198b071776f5
Joel Brobecker <brobecker@adacore.com>
2012-06-10 10:17:15 -0700

    reject_non_fast_forward: Handle output from older version of git.

--- testsuite/tests/L511-039__reject_non_fast_forward/test.py
+++ testsuite/tests/L511-039__reject_non_fast_forward/test.py
@@ -10,8 +10,10 @@ class TestRun(TestCase):
 
         self.assertTrue(p.status != 0, ex_run_image(p))
 
+        # Note: git version 1.6.5.rc2, git spells 'non-fast forward',
+        # whereas git version 1.7.1 spells 'non-fast-forward'.
         expected_out = (
-            r".*\[rejected\]\s+master\s+->\s+master\s+\(non-fast-forward\)")
+            r".*\[rejected\]\s+master\s+->\s+master\s+\(non-fast.forward\)")
 
         self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
                         ex_run_image(p))

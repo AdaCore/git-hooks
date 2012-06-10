09b1dd3ca3e540108819fa4f2d0db11275043a7d
Joel Brobecker <brobecker@adacore.com>
2012-06-10 16:11:20 -0700

    L610-006__new_branch_from_head: New testcase.

---
+++ testsuite/tests/L610-006__new_branch_from_head/test.py
@@ -0,0 +1,43 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing new branch on remote.
+
+        In this situation, release-0.1-branch is a branch containing
+        several commits attached to the HEAD of the master branch
+        (master does not have any commit that release-0.1-branch does
+        not have).
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push origin release-0.1-branch'.split())
+
+        self.assertTrue(p.status == 0, ex_run_image(p))
+
+        expected_out = (
+            r".*cvs_check: `trunk/repo/a'" +
+            r".*cvs_check: `trunk/repo/d'" +
+            r".*cvs_check: `trunk/repo/a'" +
+            r".*cvs_check: `trunk/repo/b'" +
+            r".*\[new branch\]\s+release-0\.1-branch -> release-0\.1-branch")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+        # Verify that the branch has been created in the remote
+        # repository and that it points to the expected commit.
+
+        cd('%s/bare/repo.git' % TEST_DIR)
+
+        p = Run('git show-ref -s release-0.1-branch'.split())
+
+        self.assertTrue(p.status == 0, ex_run_image(p))
+
+        expected_out = (r"dcc477c258baf8cf59db378fcc344dc962ad9a29")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+if __name__ == '__main__':
+    runtests()

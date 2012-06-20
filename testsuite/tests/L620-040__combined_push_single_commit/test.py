a7cf52434b1ef049c8be178bf05be0bf5a75f153
Joel Brobecker <brobecker@adacore.com>
2012-06-20 14:09:27 -0700

    L620-040__combined_push_single_commit: New testcase.

---
+++ testsuite/tests/L620-040__combined_push_single_commit/test.py
@@ -0,0 +1,27 @@
+from support import *
+import os
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing one single-file commit on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        os.environ['GIT_HOOKS_DEBUG_LEVEL'] = '1'
+
+        # Push master to the `origin' remote.  The delta should be one
+        # commit with one file being modified.
+        p = Run('git push origin master'.split())
+
+        self.assertTrue(p.status == 0, ex_run_image(p))
+
+        expected_out = (
+            r".*\(combined style checking\)" +
+            r".*cvs_check: `trunk/repo/a'" +
+            ".*master\s+->\s+master")
+
+        self.assertTrue(re.match(expected_out, p.out, re.DOTALL),
+                        ex_run_image(p))
+
+if __name__ == '__main__':
+    runtests()

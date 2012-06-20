7baf3fbbf133016fb01d3ea38cf8740d8d61eab0
Joel Brobecker <brobecker@adacore.com>
2012-06-20 14:39:08 -0700

    L620-042__create_scratch_dir_twice: New testcase.

---
+++ testsuite/tests/L620-042__create_scratch_dir_twice/test.py
@@ -0,0 +1,46 @@
+from support import *
+from shutil import rmtree
+import sys
+from StringIO import StringIO
+
+class TestRun(TestCase):
+    def test_git_config(self):
+        """Unit test git_config with invalid config name...
+        """
+        self.enable_unit_test()
+
+        import utils
+
+        self.assertTrue(utils.scratch_dir is None)
+
+        # Call create_scratch_dir, and verify that no warning is printed
+        # on stderr, by redirecting it to a StringIO file.
+        real_stderr = sys.stderr
+        new_stderr = StringIO()
+
+        sys.stderr = new_stderr
+        utils.create_scratch_dir()
+        sys.stderr = real_stderr
+
+        rmtree(utils.scratch_dir)
+        self.assertEqual(new_stderr.getvalue(), '',
+                         new_stderr.getvalue())
+        new_stderr.close()
+
+        # Call create_scratch_dir again, and verify that a warning is
+        # printed, this time.
+        new_stderr = StringIO()
+
+        sys.stderr = new_stderr
+        utils.create_scratch_dir()
+        sys.stderr = real_stderr
+
+        rmtree(utils.scratch_dir)
+        self.assertTrue('Unexpected second call to create_scratch_dir'
+                            in new_stderr.getvalue(),
+                         new_stderr.getvalue())
+        new_stderr.close()
+
+
+if __name__ == '__main__':
+    runtests()

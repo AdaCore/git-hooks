35bd585729bec19591a9b399493f14a87f9f9f9e
Joel Brobecker <brobecker@adacore.com>
2012-11-22 18:52:36 -0800

    Also include submodules in coverage report.
    The coverage report was only taking into account the modules
    defined in GIT_HOOKS_DIR, while we started creating submodule
    hierarchies.  This change expands the coverage analysis to
    these new modules and submodules.
    
    Move the module-list computation to after running the testsuite.
    This way, we know if there are any failures sooner rather than
    later.

--- testsuite/coverage.sh
+++ testsuite/coverage.sh
@@ -8,8 +8,6 @@ root=`dirname $0`
 
 export COVERAGE_PROCESS_START=`pwd`/coverage.rc
 export COVERAGE_DIR=`pwd`
-export GIT_HOOKS_DIR="$root/../hooks"
-export GIT_HOOKS_MODULES="$GIT_HOOKS_DIR/*.py"
 
 rm -f .coverage*
 
@@ -33,7 +31,10 @@ EOF
 
 
 $root/run-testsuite $@
+
+GIT_HOOKS_DIR="$root/../hooks"
+GIT_HOOKS_MODULES=`find $GIT_HOOKS_DIR -name '*.py' -print`
 coverage combine
-coverage report "$GIT_HOOKS_MODULES"
-coverage html "$GIT_HOOKS_MODULES"
+coverage report $GIT_HOOKS_MODULES
+coverage html $GIT_HOOKS_MODULES
 perl -pi -e "s;$GIT_HOOKS_DIR/;;" htmlcov/*

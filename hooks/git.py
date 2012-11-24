eeb24efcb2658ba9045a4212a8eda03fbd2cca2a
Joel Brobecker <brobecker@adacore.com>
2012-11-24 13:30:50 -0800

    Minor edit of the copyright header.

--- hooks/git.py
+++ hooks/git.py
@@ -1,23 +1,28 @@
 # Utility functions for git
 #
-# Copyright (C) 2008  Owen Taylor
-# Copyright (C) 2009  Red Hat, Inc
+# Derived in a very large part from the gnome git hooks, themselves
+# apparently adapted form git-bz.
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# Original copyright header:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, If not, see
-# http://www.gnu.org/licenses/.
-#
-# (These are adapted from git-bz)
+# | Copyright (C) 2008  Owen Taylor
+# | Copyright (C) 2009  Red Hat, Inc
+# |
+# | This program is free software; you can redistribute it and/or
+# | modify it under the terms of the GNU General Public License
+# | as published by the Free Software Foundation; either version 2
+# | of the License, or (at your option) any later version.
+# |
+# | This program is distributed in the hope that it will be useful,
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# | GNU General Public License for more details.
+# |
+# | You should have received a copy of the GNU General Public License
+# | along with this program; if not, If not, see
+# | http://www.gnu.org/licenses/.
+# |
+# | (These are adapted from git-bz)
 
 import os
 import re

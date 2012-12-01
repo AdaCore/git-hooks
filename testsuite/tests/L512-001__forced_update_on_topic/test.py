867e994bdb473ef7d994430190907b9a60a91da5
Joel Brobecker <brobecker@adacore.com>
2012-12-01 14:23:33 +0400

    forced_update_on_topic: Full and exact expected output match.

--- testsuite/tests/L512-001__forced_update_on_topic/test.py
+++ testsuite/tests/L512-001__forced_update_on_topic/test.py
@@ -7,19 +7,18 @@ class TestRun(TestCase):
         cd ('%s/repo' % TEST_DIR)
 
         p = Run('git push -f origin topic/new-feature'.split())
+        expected_out = """\
+remote: *** !!! WARNING: This is *NOT* a fast-forward update.
+remote: *** !!! WARNING: You may have removed some important commits.
+remote: *** cvs_check: `trunk/repo/a'
+remote: *** cvs_check: `trunk/repo/b'
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+ + a605403...14d1fa2 topic/new-feature -> topic/new-feature (forced update)
+"""
 
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*WARNING: This is \*NOT\* a fast-forward update." +
-            r".*cvs_check: `trunk/repo/a'" +
-            r".*cvs_check: `trunk/repo/b'" +
-            r".*a605403\.\.\.14d1fa2\s+" +
-                r"topic/new-feature\s+->\s+topic/new-feature\s+" +
-                r"\(forced update\)")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 if __name__ == '__main__':
     runtests()

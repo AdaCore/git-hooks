0bc6a75c8f997ba18ddc8bf51761e29a04775ee1
Joel Brobecker <brobecker@adacore.com>
2012-12-01 07:34:01 +0100

    lightweight_tag: Full and exact matching of the expected output.

--- testsuite/tests/L430-001__lightweight_tag/test.py
+++ testsuite/tests/L430-001__lightweight_tag/test.py
@@ -1,5 +1,4 @@
 from support import *
-import re
 
 class TestRun(TestCase):
     def test_push_lightweight_tag(self):
@@ -16,20 +15,27 @@ class TestRun(TestCase):
         p = Run('git push origin new-tag'.split())
         self.assertEqual(p.status, 0, p.image)
 
-        expected_out = (
-            # The email that we expect to be "sent":
-            r".*From: Test Suite <testsuite@example.com>" +
-            r".*To: commits@example.com" +
-            r".*Bcc: file-ci@gnat.com" +
-            r".*Subject: \[repo\] Created tag new-tag" +
-            r".*X-ACT-checkin: repo" +
-            r".*X-Git-Refname: refs/tags/new-tag" +
-            r".*X-Git-Oldrev: 0+" +
-            r".*X-Git-Newrev: 8b9a0d6bf08d7a983affbee3c187adadaaedec9e" +
-            # Confirmation that the new tag was created.
-            r".*\[new tag\]\s+new-tag\s+->\s+new-tag\s*")
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        expected_out = """\
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@example.com>
+remote: To: commits@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo] Created tag new-tag
+remote: X-ACT-checkin: repo
+remote: X-Git-Refname: refs/tags/new-tag
+remote: X-Git-Oldrev: 0000000000000000000000000000000000000000
+remote: X-Git-Newrev: 8b9a0d6bf08d7a983affbee3c187adadaaedec9e
+remote:
+remote: The lightweight tag 'new-tag' was created pointing to:
+remote:
+remote:  8b9a0d6... New file: a.
+To ../bare/repo.git
+ * [new tag]         new-tag -> new-tag
+"""
+
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 
 if __name__ == '__main__':

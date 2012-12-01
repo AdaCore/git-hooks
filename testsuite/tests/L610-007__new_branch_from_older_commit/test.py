436244e3c7b070b0c7fb5c00a0a2f06882f766ca
Joel Brobecker <brobecker@adacore.com>
2012-12-01 14:29:30 +0400

    new_branch_from_older_commit: Full and exact expected output match.

--- testsuite/tests/L610-007__new_branch_from_older_commit/test.py
+++ testsuite/tests/L610-007__new_branch_from_older_commit/test.py
@@ -10,18 +10,18 @@ class TestRun(TestCase):
         cd ('%s/repo' % TEST_DIR)
 
         p = Run('git push origin release-0.1-branch'.split())
+        expected_out = """\
+remote: *** cvs_check: `trunk/repo/b'
+remote: *** cvs_check: `trunk/repo/a'
+remote: *** cvs_check: `trunk/repo/c'
+remote: *** cvs_check: `trunk/repo/d'
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+ * [new branch]      release-0.1-branch -> release-0.1-branch
+"""
 
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*cvs_check: `trunk/repo/b'" +
-            r".*cvs_check: `trunk/repo/a'" +
-            r".*cvs_check: `trunk/repo/c'" +
-            r".*cvs_check: `trunk/repo/d'" +
-            r".*\[new branch\]\s+release-0\.1-branch -> release-0\.1-branch")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
         # Verify that the branch has been created in the remote
         # repository and that it points to the expected commit.
@@ -29,13 +29,13 @@ class TestRun(TestCase):
         cd('%s/bare/repo.git' % TEST_DIR)
 
         p = Run('git show-ref -s release-0.1-branch'.split())
+        expected_out = """\
+4205e52273adad6b014e19fb1cf1fe1c9b8b4089
+"""
 
         self.assertTrue(p.status == 0, p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
-        expected_out = (r"4205e52273adad6b014e19fb1cf1fe1c9b8b4089")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
 
 if __name__ == '__main__':
     runtests()

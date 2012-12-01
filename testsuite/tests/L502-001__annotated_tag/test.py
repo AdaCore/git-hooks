880b7c5b1cae7c36f477ba957431da2808788732
Joel Brobecker <brobecker@adacore.com>
2012-12-01 10:04:16 +0100

    annotated_tag: Full and exact expected output check.

--- testsuite/tests/L502-001__annotated_tag/test.py
+++ testsuite/tests/L502-001__annotated_tag/test.py
@@ -11,28 +11,32 @@ class TestRun(TestCase):
         p = Run('git push origin v0.1'.split())
         self.assertEqual(p.status, 0, p.image)
 
-        expected_out = (
-            # The email that we expect to be "sent":
-            r".*From: Test Suite <testsuite@example.com>" +
-            r".*To: testsuite@example.com" +
-            r".*Bcc: file-ci@gnat.com" +
-            r".*Subject: \[repo\] Created tag v0\.1" +
-            r".*X-ACT-checkin: repo" +
-            r".*X-Git-Refname: refs/tags/v0\.1" +
-            r".*X-Git-Oldrev: 0+" +
-            r".*X-Git-Newrev: c4c1e91cddc3d48a2aab7d454bc6537149f37dd8" +
-            r"" +
-            r".*The unsigned tag 'v0\.1' was created pointing to:" +
-            r".*8b9a0d6... New file: a\." +
-            r"" +
-            r".*Tagger: Joel Brobecker <brobecker@adacore.com>" +
-            r".*Date: Tue Jun 26 07:51:14 2012 -0700" +
-            r"" +
-            r".*This is a new tag\." +
-            # Confirmation that the new tag was created.
-            r".*\[new tag\]\s+v0.1\s+->\s+v0.1\s*")
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        expected_out = """\
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@example.com>
+remote: To: testsuite@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo] Created tag v0.1
+remote: X-ACT-checkin: repo
+remote: X-Git-Refname: refs/tags/v0.1
+remote: X-Git-Oldrev: 0000000000000000000000000000000000000000
+remote: X-Git-Newrev: c4c1e91cddc3d48a2aab7d454bc6537149f37dd8
+remote:
+remote: The unsigned tag 'v0.1' was created pointing to:
+remote:
+remote:  8b9a0d6... New file: a.
+remote:
+remote: Tagger: Joel Brobecker <brobecker@adacore.com>
+remote: Date: Tue Jun 26 07:51:14 2012 -0700
+remote:
+remote:     This is a new tag.
+To ../bare/repo.git
+ * [new tag]         v0.1 -> v0.1
+"""
+
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 
 if __name__ == '__main__':

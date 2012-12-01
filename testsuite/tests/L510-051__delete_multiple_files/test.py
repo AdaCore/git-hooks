d1a8c863f43bc97da734b452ba1494efc584d5f2
Joel Brobecker <brobecker@adacore.com>
2012-12-01 10:59:42 +0100

    delete_multiple_files: Full and exact expected output match.

--- testsuite/tests/L510-051__delete_multiple_files/test.py
+++ testsuite/tests/L510-051__delete_multiple_files/test.py
@@ -12,17 +12,23 @@ class TestRun(TestCase):
         self.set_debug_level(2)
 
         p = Run('git push origin master'.split())
-
+        expected_out = """\
+remote:   DEBUG: check_update(ref_name=refs/heads/master, old_rev=f82624871b6cfc46d5a7c5be518bc20e8f42be42, new_rev=0c702ad3051f00b1251bca7a0241a3a9bf19bf0d)
+remote: DEBUG: validate_ref_update (refs/heads/master, f82624871b6cfc46d5a7c5be518bc20e8f42be42, 0c702ad3051f00b1251bca7a0241a3a9bf19bf0d)
+remote: DEBUG: update base: f82624871b6cfc46d5a7c5be518bc20e8f42be42
+remote: DEBUG: (commit-per-commit style checking)
+remote: DEBUG: check_commit(old_rev=f82624871b6cfc46d5a7c5be518bc20e8f42be42, new_rev=0c702ad3051f00b1251bca7a0241a3a9bf19bf0d)
+remote:   DEBUG: deleted file ignored: a.adb
+remote:   DEBUG: deleted file ignored: b.adb
+remote: DEBUG: post_receive_one(ref_name=f82624871b6cfc46d5a7c5be518bc20e8f42be42
+remote:                         old_rev=0c702ad3051f00b1251bca7a0241a3a9bf19bf0d
+remote:                         new_rev=refs/heads/master)
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+   f826248..0c702ad  master -> master
+"""
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*check_commit.*new_rev=0c702ad3051f00b1251bca7a0241a3a9bf19bf0d" +
-            r".*deleted file ignored: a\.adb" +
-            r".*deleted file ignored: b\.adb" +
-            r".*\s+f826248\.\.0c702ad\s+master\s+->\s+master")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 if __name__ == '__main__':
     runtests()

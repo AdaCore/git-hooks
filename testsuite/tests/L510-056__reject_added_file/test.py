a37e41aed9895ff63729922b5d6622f229ed963a
Joel Brobecker <brobecker@adacore.com>
2012-12-01 14:09:35 +0400

    reject_added_file: Full and exact expected output check.

--- testsuite/tests/L510-056__reject_added_file/test.py
+++ testsuite/tests/L510-056__reject_added_file/test.py
@@ -6,27 +6,24 @@ class TestRun(TestCase):
         """
         cd ('%s/repo' % TEST_DIR)
 
-        p = Run('git push origin master'.split())
-
         # The push should fail, because the pre-commit checker will
         # refuse one of the updates.
+        p = Run('git push origin master'.split())
+        expected_out = """\
+remote: *** cvs_check: `trunk/repo/b'
+remote: *** cvs_check: `trunk/repo/pck.adb'
+remote: *** pre-commit check failed for file `pck.ads' at commit: 16c509ed1a0f8b558f8ed9664a06b8cf905fc6b2
+remote: *** cvs_check: `trunk/repo/pck.ads'
+remote: *** ERROR: style-check error detected for file: `trunk/repo/pck.ads'.
+remote: *** ERROR: Copyright year in header is not up to date
+remote: error: hook declined to update refs/heads/master
+To ../bare/repo.git
+ ! [remote rejected] master -> master (hook declined)
+error: failed to push some refs to '../bare/repo.git'
+"""
+
         self.assertTrue(p.status != 0, p.image)
-
-        expected_out = (
-            r".*cvs_check: `trunk/repo/b'" +
-            r".*cvs_check: `trunk/repo/pck.adb'" +
-            r".*pre-commit check failed for file `pck.ads' at commit: " +
-                "16c509ed1a0f8b558f8ed9664a06b8cf905fc6b2" +
-            r".*cvs_check: `trunk/repo/pck.ads'" +
-            r".*ERROR: style-check error detected for file: " +
-                r"`trunk/repo/pck\.ads'\." +
-            r".*ERROR: Copyright year in header is not up to date" +
-            r".*error: hook declined to update refs/heads/master" +
-            r".*! \[remote rejected\] master -> master \(hook declined\)" +
-            r".*error: failed to push some refs to '\.\./bare/repo.git'")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 if __name__ == '__main__':
     runtests()

7c863738b5a4a6d52a7156166e1868d954f97230
Joel Brobecker <brobecker@adacore.com>
2012-12-01 14:33:04 +0400

    new_branch_is_older_commit: Full and exact expected output match.

--- testsuite/tests/L610-009__new_branch_is_older_commit/test.py
+++ testsuite/tests/L610-009__new_branch_is_older_commit/test.py
@@ -10,14 +10,14 @@ class TestRun(TestCase):
         cd ('%s/repo' % TEST_DIR)
 
         p = Run('git push origin release-0.1-branch'.split())
+        expected_out = """\
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+ * [new branch]      release-0.1-branch -> release-0.1-branch
+"""
 
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*\[new branch\]\s+release-0\.1-branch -> release-0\.1-branch")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
         # Verify that the branch has been created in the remote
         # repository and that it points to the expected commit.
@@ -25,13 +25,13 @@ class TestRun(TestCase):
         cd('%s/bare/repo.git' % TEST_DIR)
 
         p = Run('git show-ref -s release-0.1-branch'.split())
+        expected_out = """\
+4a325b31f594b1dc2c66ac15c4b6b68702bd0cdf
+"""
 
         self.assertTrue(p.status == 0, p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
-        expected_out = (r"4a325b31f594b1dc2c66ac15c4b6b68702bd0cdf")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
 
 if __name__ == '__main__':
     runtests()

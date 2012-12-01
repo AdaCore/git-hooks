ff99dd1833ad2eabbb42912e5a3dfb7ee302a2dc
Joel Brobecker <brobecker@adacore.com>
2012-12-01 14:14:38 +0400

    l_tag_before_commit: Full and exact expected output match.

--- testsuite/tests/L511-038__l_tag_before_commit/test.py
+++ testsuite/tests/L511-038__l_tag_before_commit/test.py
@@ -13,44 +13,69 @@ class TestRun(TestCase):
         # Scenario: The user made some changes, and then committed them
         # in his repo. Then created a lightweight tag (release-0.1a).
         # Next, he pushes the lightweight tag before having pushed his
-        # new commit.  What the commit hooks should do in this case
-        # is just accept the lightweight tag, but not check the commits
-        # (see later for when these commits should be checked).
+        # new commit.
         #
         # Note: The remote repository has been configured to allow
         # unnanotated tags.
 
         p = Run('git push origin version-0.1a'.split())
+        expected_out = """\
+remote: DEBUG: validate_ref_update (refs/tags/version-0.1a, 0000000000000000000000000000000000000000, 4f0f08f46daf6f5455cf90cdc427443fe3b32fa3)
+remote: DEBUG: update base: 426fba3571947f6de7f967e885a3168b9df7004a
+remote: DEBUG: (commit-per-commit style checking)
+remote: DEBUG: check_commit(old_rev=426fba3571947f6de7f967e885a3168b9df7004a, new_rev=4f0f08f46daf6f5455cf90cdc427443fe3b32fa3)
+remote: *** cvs_check: `trunk/repo/a'
+remote: *** cvs_check: `trunk/repo/b'
+remote: *** cvs_check: `trunk/repo/c'
+remote: DEBUG: post_receive_one(ref_name=0000000000000000000000000000000000000000
+remote:                         old_rev=4f0f08f46daf6f5455cf90cdc427443fe3b32fa3
+remote:                         new_rev=refs/tags/version-0.1a)
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@adacore.com>
+remote: To: git-hooks-ci@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo] Created tag version-0.1a
+remote: X-ACT-checkin: repo
+remote: X-Git-Refname: refs/tags/version-0.1a
+remote: X-Git-Oldrev: 0000000000000000000000000000000000000000
+remote: X-Git-Newrev: 4f0f08f46daf6f5455cf90cdc427443fe3b32fa3
+remote:
+remote: The lightweight tag 'version-0.1a' was created pointing to:
+remote:
+remote:  4f0f08f... Minor modifications.
+To ../bare/repo.git
+ * [new tag]         version-0.1a -> version-0.1a
+"""
 
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*\s+\[new tag\]\s+version-0.1a\s+->\s+version-0.1a")
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
-
-        # Also verify from the output that none of the commits get
-        # checked.  For that, rely on the check_commit debug trace.
-
-        self.assertFalse(re.match(r"DEBUG: check_commit\(", p.cmd_out, re.DOTALL),
-                         p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
         # Next, push the changes. Make sure that the commit gets checked.
+        # ??? There is a limitation that causes the hooks to re-check
+        # a commit which is not new (accessible throught the tag created
+        # above, but not from any of the branches).
 
         p = Run('git push origin master'.split())
+        expected_out = """\
+remote: DEBUG: validate_ref_update (refs/heads/master, 426fba3571947f6de7f967e885a3168b9df7004a, 4f0f08f46daf6f5455cf90cdc427443fe3b32fa3)
+remote: DEBUG: update base: 426fba3571947f6de7f967e885a3168b9df7004a
+remote: DEBUG: (commit-per-commit style checking)
+remote: DEBUG: check_commit(old_rev=426fba3571947f6de7f967e885a3168b9df7004a, new_rev=4f0f08f46daf6f5455cf90cdc427443fe3b32fa3)
+remote: *** cvs_check: `trunk/repo/a'
+remote: *** cvs_check: `trunk/repo/b'
+remote: *** cvs_check: `trunk/repo/c'
+remote: DEBUG: post_receive_one(ref_name=426fba3571947f6de7f967e885a3168b9df7004a
+remote:                         old_rev=4f0f08f46daf6f5455cf90cdc427443fe3b32fa3
+remote:                         new_rev=refs/heads/master)
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+   426fba3..4f0f08f  master -> master
+"""
 
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*check_commit(.*" \
-                + "new_rev=4f0f08f46daf6f5455cf90cdc427443fe3b32fa3)"
-            r".*cvs_check: `trunk/repo/a'" +
-            r".*cvs_check: `trunk/repo/b'" +
-            r".*cvs_check: `trunk/repo/c'" +
-            r".*\s+426fba3\.\.4f0f08f\s+master\s+->\s+master")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 if __name__ == '__main__':
     runtests()

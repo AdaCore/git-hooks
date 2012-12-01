542d5423d3d2bc132f96af5ff92c2eb75867caa3
Joel Brobecker <brobecker@adacore.com>
2012-12-01 07:37:13 +0100

    delete_l_tag: Full and exact expected output match.

--- testsuite/tests/L430-003__delete_l_tag/test.py
+++ testsuite/tests/L430-003__delete_l_tag/test.py
@@ -1,5 +1,4 @@
 from support import *
-import re
 
 class TestRun(TestCase):
     def test_delete_lightweight_tag(self):
@@ -10,18 +9,28 @@ class TestRun(TestCase):
         p = Run('git push origin :some-tag'.split())
         self.assertEqual(p.status, 0, p.image)
 
-        expected_out = (
-            r".*From: Test Suite <testsuite@example.com>" +
-            r".*To: something-ci@example.com" +
-            r".*Bcc: file-ci@gnat.com" +
-            r".*Subject: \[repo\] Deleted tag some-tag" +
-            r".*X-ACT-checkin: repo" +
-            r".*X-Git-Refname: refs/tags/some-tag" +
-            r".*X-Git-Oldrev: 8b9a0d6bf08d7a983affbee3c187adadaaedec9e" +
-            r".*X-Git-Newrev: 0000000000000000000000000000000000000000" +
-            r".*-\s+\[deleted\]\s+some-tag")
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        expected_out = """\
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@example.com>
+remote: To: something-ci@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo] Deleted tag some-tag
+remote: X-ACT-checkin: repo
+remote: X-Git-Refname: refs/tags/some-tag
+remote: X-Git-Oldrev: 8b9a0d6bf08d7a983affbee3c187adadaaedec9e
+remote: X-Git-Newrev: 0000000000000000000000000000000000000000
+remote:
+remote: The lightweight tag 'some-tag' was deleted.
+remote: It previously pointed to:
+remote:
+remote:  8b9a0d6... New file: a.
+To ../bare/repo.git
+ - [deleted]         some-tag
+"""
+
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 if __name__ == '__main__':
     runtests()

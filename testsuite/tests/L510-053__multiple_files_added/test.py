010822e5398531e095629e375a6219a04a7e0430
Joel Brobecker <brobecker@adacore.com>
2012-12-01 11:06:12 +0100

    multiple_files_added: Full and exact expected output match.

--- testsuite/tests/L510-053__multiple_files_added/test.py
+++ testsuite/tests/L510-053__multiple_files_added/test.py
@@ -7,17 +7,17 @@ class TestRun(TestCase):
         cd ('%s/repo' % TEST_DIR)
 
         p = Run('git push origin master'.split())
+        expected_out = """\
+remote: *** cvs_check: `trunk/repo/pck.adb'
+remote: *** cvs_check: `trunk/repo/pck.ads'
+remote: *** cvs_check: `trunk/repo/types.ads'
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+   f826248..c699b49  master -> master
+"""
 
         self.assertTrue(p.status == 0, p.image)
-
-        expected_out = (
-            r".*cvs_check: `trunk/repo/pck.adb'" +
-            r".*cvs_check: `trunk/repo/pck.ads'" +
-            r".*cvs_check: `trunk/repo/types.ads'" +
-            ".*master\s+->\s+master")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 if __name__ == '__main__':
     runtests()

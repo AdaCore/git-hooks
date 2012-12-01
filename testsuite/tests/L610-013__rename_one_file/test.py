66332797b13ee1fa92d249afd599fb24709f4f66
Joel Brobecker <brobecker@adacore.com>
2012-12-01 14:38:07 +0400

    rename_one_file: Full and exact expected output match.

--- testsuite/tests/L610-013__rename_one_file/test.py
+++ testsuite/tests/L610-013__rename_one_file/test.py
@@ -15,16 +15,25 @@ class TestRun(TestCase):
         # Push master to the `origin' remote.  The delta should be one
         # commit with one file being modified.
         p = Run('git push origin master'.split())
+        expected_out = """\
+remote:   DEBUG: check_update(ref_name=refs/heads/master, old_rev=a60540361d47901d3fe254271779f380d94645f7, new_rev=6a48cdab9b100506a387a8398af4751b33a4bfd0)
+remote: DEBUG: validate_ref_update (refs/heads/master, a60540361d47901d3fe254271779f380d94645f7, 6a48cdab9b100506a387a8398af4751b33a4bfd0)
+remote: DEBUG: update base: a60540361d47901d3fe254271779f380d94645f7
+remote: DEBUG: (commit-per-commit style checking)
+remote: DEBUG: check_commit(old_rev=a60540361d47901d3fe254271779f380d94645f7, new_rev=6a48cdab9b100506a387a8398af4751b33a4bfd0)
+remote:   DEBUG: deleted file ignored: a
+remote: *** cvs_check: `trunk/repo/b'
+remote: DEBUG: post_receive_one(ref_name=a60540361d47901d3fe254271779f380d94645f7
+remote:                         old_rev=6a48cdab9b100506a387a8398af4751b33a4bfd0
+remote:                         new_rev=refs/heads/master)
+remote: *** email notification for new commits not implemented yet.
+To ../bare/repo.git
+   a605403..6a48cda  master -> master
+"""
 
         self.assertTrue(p.status == 0, p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
-        expected_out = (
-            r".*DEBUG: deleted file ignored: a" +
-            r".*cvs_check: `trunk/repo/b'" +
-            ".*master\s+->\s+master")
-
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
 
 if __name__ == '__main__':
     runtests()

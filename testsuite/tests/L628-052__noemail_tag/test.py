7860b6201f5938bd8269df95fbbb09f09452175d
Joel Brobecker <brobecker@adacore.com>
2012-12-02 19:24:54 +0400

    noemail_tag: Full and exact expected output match.

--- testsuite/tests/L628-052__noemail_tag/test.py
+++ testsuite/tests/L628-052__noemail_tag/test.py
@@ -1,5 +1,4 @@
 from support import *
-import re
 
 class TestRun(TestCase):
     def test_push_annotated_tag(self):
@@ -9,23 +8,17 @@ class TestRun(TestCase):
 
         # Try pushing tag v0.1.
         p = Run('git push origin v0.1'.split())
+        expected_out = """\
+remote: ---------------------------------------------------------------------------
+remote: --  The hooks.noemails config parameter contains `refs/tags/v0.1'.
+remote: --  Commit emails will therefore not be sent.
+remote: ---------------------------------------------------------------------------
+To ../bare/repo.git
+ * [new tag]         v0.1 -> v0.1
+"""
+
         self.assertEqual(p.status, 0, p.image)
-
-        expected_out = (
-            # The warning explaining that emails are not going to be sent.
-            r".*The hooks\.noemails config parameter contains"
-            r".*Commit emails will therefore not be sent"
-
-            # Confirmation that the new tag was created.
-            r".*\[new tag\]\s+v0.1\s+->\s+v0.1\s*")
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
-
-        # Check the output for anything that might look like an email
-        # was sent.
-        self.assertFalse('From:' in p.cmd_out, p.image)
-        self.assertFalse('To:' in p.cmd_out, p.image)
-        self.assertFalse('Subject:' in p.cmd_out, p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 
 if __name__ == '__main__':

39839c4b8d911933390d2e9ed5145b143cc55148
Joel Brobecker <brobecker@adacore.com>
2012-12-02 19:21:54 +0400

    update_tag: Full and exact expected output matching.

--- testsuite/tests/L628-049__update_tag/test.py
+++ testsuite/tests/L628-049__update_tag/test.py
@@ -1,5 +1,4 @@
 from support import *
-import re
 
 class TestRun(TestCase):
     def test_push_tag(self):
@@ -14,48 +13,58 @@ class TestRun(TestCase):
         # Push "full-tag". This tag has a new value, different from
         # that is on the remote.  We should get an email notification,
         # and a warning about how bad it is to do that.
-        expected_out = (
-            # The warning about the dangers of modifying a published tag.
-            r".*IMPORTANT NOTICE:" +
-            r'.*You just updated the "full-tag" tag as follow:' +
-            r".*  old SHA1: a69eaaba59ea6d7574a9c5437805a628ea652c8e" +
-            r".*  new SHA1: 17b9d4acf8505cd1da487ad62e37819b93779a27" +
-
-            # This push introduces one new commit, so we expect the
-            # associated cvs_check traces as well.
-            r".*DEBUG: check_commit\(" +
-                "old_rev=354383fa06047ef8053782410c221341e4b07ec4, " +
-                "new_rev=8c0b4151f9f41efcaeb70ea6f91158e4605aaeda\)" +
-            r".*cvs_check: `trunk/repo/bar\.c'" +
-            r".*cvs_check: `trunk/repo/foo'" +
-
-            # The header of the notification email that should be sent...
-            r".*From: Test Suite <testsuite@example.com>" +
-            r".*To: repo@example.com" +
-            r".*Bcc: file-ci@gnat.com" +
-            r".*Subject: \[repo\] Updated tag full-tag" +
-            r".*X-ACT-checkin: repo" +
-            r".*X-Git-Refname: refs/tags/full-tag" +
-            r".*X-Git-Oldrev: a69eaaba59ea6d7574a9c5437805a628ea652c8e" +
-            r".*X-Git-Newrev: 17b9d4acf8505cd1da487ad62e37819b93779a27" +
-
-            # The body of that email...
-            r".*The signed tag 'full-tag' was updated" +
-            r".*8c0b415\.\.\. Added bar\.c, and updated foo\." +
-            r".*It previously pointed to:" +
-            r".*354383f\.\.\. Initial commit\." +
-            r".*Tagger: Joel Brobecker <brobecker@adacore.com>" +
-            r".*Date: Thu Jun 28 11:50:36 2012 -0700" +
-            r".*Tag a commit that makes more sense\." +
-
-            # Output from git that confirms that the tag was updated.
-            r".*a69eaab\.\.17b9d4a\s+full-tag\s+->\s+full-tag"
-            )
-
         p = Run('git push origin full-tag'.split())
+        expected_out = """\
+remote: DEBUG: validate_ref_update (refs/tags/full-tag, a69eaaba59ea6d7574a9c5437805a628ea652c8e, 17b9d4acf8505cd1da487ad62e37819b93779a27)
+remote: *** ---------------------------------------------------------------
+remote: *** --  IMPORTANT NOTICE:
+remote: *** --
+remote: *** --  You just updated the "full-tag" tag as follow:
+remote: *** --    old SHA1: a69eaaba59ea6d7574a9c5437805a628ea652c8e
+remote: *** --    new SHA1: 17b9d4acf8505cd1da487ad62e37819b93779a27
+remote: *** --
+remote: *** -- Other developers pulling from this repository will not
+remote: *** -- get the new tag. Assuming this update was deliberate,
+remote: *** -- notifying all known users of the update is recommended.
+remote: *** ---------------------------------------------------------------
+remote: DEBUG: update base: 354383fa06047ef8053782410c221341e4b07ec4
+remote: DEBUG: (commit-per-commit style checking)
+remote: DEBUG: check_commit(old_rev=354383fa06047ef8053782410c221341e4b07ec4, new_rev=8c0b4151f9f41efcaeb70ea6f91158e4605aaeda)
+remote: *** cvs_check: `trunk/repo/bar.c'
+remote: *** cvs_check: `trunk/repo/foo'
+remote: DEBUG: post_receive_one(ref_name=a69eaaba59ea6d7574a9c5437805a628ea652c8e
+remote:                         old_rev=17b9d4acf8505cd1da487ad62e37819b93779a27
+remote:                         new_rev=refs/tags/full-tag)
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@example.com>
+remote: To: repo@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo] Updated tag full-tag
+remote: X-ACT-checkin: repo
+remote: X-Git-Refname: refs/tags/full-tag
+remote: X-Git-Oldrev: a69eaaba59ea6d7574a9c5437805a628ea652c8e
+remote: X-Git-Newrev: 17b9d4acf8505cd1da487ad62e37819b93779a27
+remote:
+remote: The signed tag 'full-tag' was updated to point to:
+remote:
+remote:  8c0b415... Added bar.c, and updated foo.
+remote:
+remote: It previously pointed to:
+remote:
+remote:  354383f... Initial commit.
+remote:
+remote: Tagger: Joel Brobecker <brobecker@adacore.com>
+remote: Date: Thu Jun 28 11:50:36 2012 -0700
+remote:
+remote:     Tag a commit that makes more sense.
+To ../bare/repo.git
+   a69eaab..17b9d4a  full-tag -> full-tag
+"""
+
         self.assertEqual(p.status, 0, p.image)
-        self.assertTrue(re.match(expected_out, p.cmd_out, re.DOTALL),
-                        p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
 
 
 if __name__ == '__main__':

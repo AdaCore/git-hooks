23457eb849171fbbe3ba9faf91b65b168ee29bbc
Joel Brobecker <brobecker@adacore.com>
2012-12-10 10:12:48 +0400

    LC10-004__push_many_commits: New testcase.

---
+++ testsuite/tests/LC10-004__push_many_commits/test.py
@@ -0,0 +1,26 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_too_many_new_commits_on_master(self):
+        """Try pushing too many new commits on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        # Push master to the `origin' remote.  The remote should
+        # reject it saying that there are too many new commits.
+        p = Run('git push origin master'.split())
+        expected_out = """\
+remote: ---------------------------------------------------------------------------
+remote: --  The hooks.noemails config parameter contains `refs/heads/master'.
+remote: --  Commit emails will therefore not be sent.
+remote: ---------------------------------------------------------------------------
+To ../bare/repo.git
+   d065089..4ca9852  master -> master
+"""
+
+        self.assertEqual(p.status, 0, p.image)
+        self.assertEqual(expected_out, p.cmd_out, p.image)
+
+
+if __name__ == '__main__':
+    runtests()

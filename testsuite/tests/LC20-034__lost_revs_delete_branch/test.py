484827379bc4a849808445ab5ad4109bd4b8cb79
Joel Brobecker <brobecker@adacore.com>
2012-12-20 18:28:14 +0400

    LC20-034__lost_revs_delete_branch: New testcase.

---
+++ testsuite/tests/LC20-034__lost_revs_delete_branch/test.py
@@ -0,0 +1,43 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_delete_branch(self):
+        """Test deleting topic branch causing commits to be lost.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        # Push master to the `origin' remote.  The delta should be one
+        # commit with one file being modified.
+        p = Run('git push origin :topic/experiment1'.split())
+        expected_out = """\
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@adacore.com>
+remote: To: git-hooks-ci@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo] Deleted branch topic/experiment1
+remote: X-ACT-checkin: repo
+remote: X-Git-Refname: refs/heads/topic/experiment1
+remote: X-Git-Oldrev: a60540361d47901d3fe254271779f380d94645f7
+remote: X-Git-Newrev: 0000000000000000000000000000000000000000
+remote:
+remote: The branch 'topic/experiment1' was deleted.
+remote: It previously pointed to:
+remote:
+remote:  a605403... Updated a.
+remote:
+remote: !!! WARNING: THE FOLLOWING COMMITS ARE NO LONGER ACCESSIBLE (LOST):
+remote: -------------------------------------------------------------------
+remote:
+remote:   a605403... Updated a.
+To ../bare/repo.git
+ - [deleted]         topic/experiment1
+"""
+
+        self.assertTrue(p.status == 0, p.image)
+
+        self.assertEqual(expected_out, p.cmd_out, p.image)
+
+if __name__ == '__main__':
+    runtests()

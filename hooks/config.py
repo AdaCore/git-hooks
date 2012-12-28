e733c1099a51b060a4112b4f6ab851089a4f03ff
Joel Brobecker <brobecker@adacore.com>
2012-12-28 11:37:16 +0400

    Reimplement config.py to call "git config" only once.
    This patch currently removes the 'type' info from the GIT_CONFIG_OPTS
    objects, but this will be put back later in a slightly more powerful
    form (ie: the module will be using this type info to convert the
    config option into the right type, instead of just passing it to
    "git config" to validate the value).
    
    For LC27-007.

--- hooks/config.py
+++ hooks/config.py
@@ -1,70 +1,86 @@
-from git import git, CalledProcessError
+from git import git
 
 # A dictionary of all git config names that this module can query.
 #   - The key used for this table is the config name.
 #   - The value is another dictionary containing the following keys.
-#       x 'type': The value is a string, and represents the type of
-#                 of value our config parameter.  It should be either
-#                 "bool", "int", "bool_or_int" or "path".
-#                 This element is optional, in which case the type
-#                 is assumed to be a simple string.
-#       x 'default': The default value for this config option.
+#       + 'default': The default value for this config option.
 
-GIT_CONFIG_OPTS = {
-    'hooks.allow-delete-tag' : { 'type' : 'bool', 'default' : 'false' },
-    'hooks.allow-non-fast-forward' : {            'default' : ''},
-    'hooks.allow-lightweight-tag' :
-                               { 'type' : 'bool', 'default' : 'false' },
-    'hooks.combined-style-checking' :
-                               { 'type' : 'bool', 'default' : 'false' },
-    'hooks.debug-level'      : { 'type' : 'int',  'default' : '0' },
-    'hooks.from-domain'      : {                  'default' : None},
-    'hooks.mailinglist'      : {                  'default' : None},
-    'hooks.max-commit-emails': { 'type' : 'int',  'default' : '100' },
-    'hooks.no-emails'        : {                  'default' : ''},
-    'hooks.no-precommit-check' : {                'default' : ''},
-    'hooks.tn-required'      : { 'type' : 'bool', 'default' : 'false' },
+GIT_CONFIG_OPTS = \
+    {'hooks.allow-delete-tag':         {'default': 'false'},
+     'hooks.allow-non-fast-forward':   {'default': ''},
+     'hooks.allow-lightweight-tag':    {'default': 'false'},
+     'hooks.combined-style-checking':  {'default': 'false'},
+     'hooks.debug-level':              {'default': '0'},
+     'hooks.from-domain':              {'default': None},
+     'hooks.mailinglist':              {'default': None},
+     'hooks.max-commit-emails':        {'default': '100'},
+     'hooks.no-emails':                {'default': ''},
+     'hooks.no-precommit-check':       {'default': ''},
+     'hooks.tn-required':              {'default': 'false'},
 
-    # The following options are for testing purposes only, and should
-    # never be used in an operational repository.
-    'hooks.bcc-file-ci'      : { 'type' : 'bool', 'default' : 'true' },
-}
+     # The following options are for testing purposes only, and should
+     # never be used in an operational repository.
+     'hooks.bcc-file-ci':              {'default': 'true'},
+    }
 
 # The maximum number of characters from a commit's subject
 # to be used as part of the subject of emails describing
 # the commit.
 SUBJECT_MAX_SUBJECT_CHARS = 100
 
+
 class UnsupportedOptionName(Exception):
     """An exception raised when trying to lookup an unsupported option name.
     """
     pass
 
+
+# A map containing the value of the various git config options.
+# Lazy initialized.
+#
+# It includes all options listed in GIT_CONFIG_OPTS; if the project
+# does not set a given option, then the default value is used in
+# its place.
+__git_config_map = None
+
+
 def git_config(option_name):
-    """Get the repository or global option.
-
-    Only the option names in GIT_CONFIG_OPTS are supported, and
-    UnsupportedOptionName is raised if an unsupported option_name
-    is passed.
-
-    If the option is unset, return the default value, as provided
-    by GIT_CONFIG_OPTS.
+    """Return the git config value for option_name.
 
     PARAMETERS
-        option_name: The name of the config option.
+        option_name: The name of the git config option to query.
     """
+    global __git_config_map
+
     if option_name not in GIT_CONFIG_OPTS:
         raise UnsupportedOptionName(option_name)
-    option_data = GIT_CONFIG_OPTS[option_name]
 
-    kwargs = {}
-    if 'type' in option_data:
-        kwargs[option_data['type']] = True
+    if __git_config_map is None:
+        initialize_git_config_map()
 
-    try:
-        option_val = git.config(option_name, **kwargs)
-    except CalledProcessError:
-        # This option is probably not set in the config file.  Return
-        # the default.
-        option_val = option_data['default']
-    return option_val
+    return __git_config_map[option_name]
+
+
+def initialize_git_config_map():
+    """Initialize the __git_config_map global.
+    """
+    global __git_config_map
+
+    # Get the currently defined config values, all in one go.
+    all_configs = git.config('-l', _split_lines=True)
+    all_configs_map = dict([config.split('=', 1) for config in all_configs])
+
+    # Populate the __git_config_map dictionary...
+    __git_config_map = {}
+    for config_name in GIT_CONFIG_OPTS.keys():
+        config_info = GIT_CONFIG_OPTS[config_name]
+
+        # Get the config value from either the all_configs_map
+        # if defined, or else from the default value.
+        if config_name in all_configs_map:
+            config_val = all_configs_map[config_name]
+        else:
+            config_val = GIT_CONFIG_OPTS[config_name]['default']
+
+        # Finally, save the config value if __git_config_map
+        __git_config_map[config_name] = config_val

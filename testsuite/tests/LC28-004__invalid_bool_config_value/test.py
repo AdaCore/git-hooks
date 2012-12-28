0c5f1739756cb79dbd301e4c64b1aee652b00267
Joel Brobecker <brobecker@adacore.com>
2012-12-28 14:20:17 +0400

    LC28-004__invalid_bool_config_value: New testcase.

---
+++ testsuite/tests/LC28-004__invalid_bool_config_value/test.py
@@ -0,0 +1,27 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing one single-file commit on master.
+
+        The purpose of this testcase is to test combined style checking.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        # Push master to the `origin' remote.  The update should
+        # fail because hooks.combined-style-checking does not have
+        # a valid value (simulating a typo).
+        p = Run('git push origin master'.split())
+        expected_out = """\
+remote: *** Invalid hooks.combined-style-checking value: frue (must be boolean)
+remote: error: hook declined to update refs/heads/master
+To ../bare/repo.git
+ ! [remote rejected] master -> master (hook declined)
+error: failed to push some refs to '../bare/repo.git'
+"""
+
+        self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, expected_out)
+
+if __name__ == '__main__':
+    runtests()

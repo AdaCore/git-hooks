99eadb0845c931475ae5f1c35f1494b8b63cfbea
Joel Brobecker <brobecker@adacore.com>
2012-12-30 09:02:52 +0400

    LC29-002__multiple_no_precommit_check: New testcase.

---
+++ testsuite/tests/LC29-002__multiple_no_precommit_check/cvs_check.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+"""A dummy cvs_check program that fails all files.
+"""
+import sys
+
+# To help with testing, print a trace containing the name of the file
+# that is being checked.
+print >> sys.stderr, "cvs_check: `%s'" % sys.argv[1]
+
+# Fail the style-check for the following files:
+print >> sys.stderr, 'ERROR: style-check should not be performed.'
+sys.exit(1)
+

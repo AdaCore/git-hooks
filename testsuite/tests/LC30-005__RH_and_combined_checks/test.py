736f93fcda4a87128eaa3656f857ec5058de6a3c
Joel Brobecker <brobecker@adacore.com>
2012-12-31 10:49:12 +0400

    LC30-005__RH_and_combined_checks: New testcase.

---
+++ testsuite/tests/LC30-005__RH_and_combined_checks/test.py
@@ -0,0 +1,30 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing multiple commits on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push origin master'.split())
+        expected_out = """\
+remote: *** The following commit is missing a ticket number inside
+remote: *** its revision history.  If the change is sufficiently
+remote: *** minor that a ticket number is not meaningful, please use
+remote: *** either the word "Minor" or the "(no-tn-check)" string
+remote: *** in place of a ticket number.
+remote: ***
+remote: *** commit 4273f9ac2433552585240d383b3157b8ada2135c
+remote: *** Subject: Small modifications.
+remote: error: hook declined to update refs/heads/master
+To ../bare/repo.git
+ ! [remote rejected] master -> master (hook declined)
+error: failed to push some refs to '../bare/repo.git'
+"""
+
+        self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, expected_out)
+
+
+if __name__ == '__main__':
+    runtests()

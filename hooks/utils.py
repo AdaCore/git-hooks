e8c2e70f34e67ddfa0e974168c2c69767eebcc58
Joel Brobecker <brobecker@adacore.com>
2013-12-26 15:13:20 +0400

    utils.py: Fix pep8 violoations

--- hooks/utils.py
+++ hooks/utils.py
@@ -14,6 +14,7 @@ from errors import InvalidUpdate
 #
 ############################################################################
 
+
 def get_user_name():
     """Return the user name (in the Unix sense: The account name).
     """
@@ -50,6 +51,7 @@ def get_user_full_name():
 # session, and to delete it at the end.
 
 scratch_dir = None
+
 
 def create_scratch_dir():
     """Create a temporary directory.

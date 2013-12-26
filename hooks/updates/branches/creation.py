ff3ae546cd8c38825666d5da3b6824a3da97caef
Joel Brobecker <brobecker@adacore.com>
2013-12-26 14:53:11 +0400

    updates/branches/creation.py: Fix pep8 violations.

--- hooks/updates/branches/creation.py
+++ hooks/updates/branches/creation.py
@@ -9,6 +9,7 @@ The branch '%(short_ref_name)s' was created pointing to:
 
  %(commit_oneline)s"""
 
+
 class BranchCreation(BranchUpdate):
     """Update class for branch creation.
 
@@ -23,9 +24,9 @@ class BranchCreation(BranchUpdate):
         subject = "[%s] Created branch %s" % (self.email_info.project_name,
                                               self.short_ref_name)
 
-        update_info = {'short_ref_name' : self.short_ref_name,
-                       'commit_oneline' : commit_oneline(self.new_rev),
-                      }
+        update_info = {'short_ref_name': self.short_ref_name,
+                       'commit_oneline': commit_oneline(self.new_rev),
+                       }
         body = BRANCH_CREATION_EMAIL_BODY_TEMPLATE % update_info
         if branch_summary_of_changes_needed(self.added_commits,
                                             self.lost_commits):

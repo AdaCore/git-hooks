4572a040f0170817760cbb4a277ca8ada4843b2c
Joel Brobecker <brobecker@adacore.com>
2014-12-21 16:38:07 -0500

    [testsuite] mailinglist_script: Slight email_to.py script improvement.
    This is mostly to have a script that's closer to what we intend to use
    with the real binutils-gdb.py repository.  Adjust the expected output
    accordingly.

--- testsuite/tests/NC07-001__mailinglist_script/email_to.py
+++ testsuite/tests/NC07-001__mailinglist_script/email_to.py
@@ -1,21 +1,23 @@
 #! /usr/bin/env python
 import sys
 
-BFD_ML = 'bfd-cvs@example.com'
-GDB_ML = 'gdb-cvs@example.com'
-EVERYONE = (BFD_ML, GDB_ML)
+ML_MAP = {'bfd': 'bfd-cvs@example.com',
+          'gdb': 'gdb-cvs@example.com',
+          }
+
+EVERYONE = set(ML_MAP[ml_key] for ml_key in ML_MAP)
 
 OWNER_MAP = (
-    ('bfd/', BFD_ML),
-    ('opcode/', BFD_ML),
-    ('gdb/', GDB_ML),
+    ('bfd/', 'bfd'),
+    ('opcode/', 'bfd'),
+    ('gdb/', 'gdb'),
     )
 
 
 def ml_from_filename(filename):
-    for (path, ml) in OWNER_MAP:
+    for (path, ml_key) in OWNER_MAP:
         if filename.startswith(path):
-            return ml
+            return ML_MAP[ml_key]
     # Not found in map, it is a common file.
     return EVERYONE
 
@@ -26,7 +28,7 @@ for filename in sys.stdin:
         result.add(ml)
     else:
         result.update(ml)
-    if len(result) >= 2:
+    if len(result) >= len(EVERYONE):
         # We have iterated over enough entries to know already
         # that we have selected all possible recipients. So
         # stop now.
@@ -36,4 +38,4 @@ if not result:
     # No files given, return EVERYONE
     result = EVERYONE
 
-print '\n'.join(result)
+print '\n'.join(sorted(result))

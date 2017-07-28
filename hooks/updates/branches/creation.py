d607c1c43293e0a1a3cedfa3bd3ad1598e3ad97a
Joel Brobecker <brobecker@adacore.com>
2017-07-28 16:16:26 -0700

    reject a branch creation if gerrit.defaultbranch is set improperly
    This patch rejects a branch creation if it looks like the user
    forgot to update the gerrit.defaultbranch config in the .gitreview
    file. This is to prevent some fairly confusing behavior when
    we forgot..,
    
    For P426-022

--- hooks/updates/branches/creation.py
+++ hooks/updates/branches/creation.py
@@ -1,6 +1,7 @@
 """Handling of branch creation."""
 
-from git import commit_oneline
+from errors import InvalidUpdate
+from git import commit_oneline, git, file_exists
 from updates.branches import branch_summary_of_changes_needed
 from updates.branches.update import BranchUpdate
 
@@ -18,6 +19,15 @@ class BranchCreation(BranchUpdate):
         some of the abstract methods would be identical.  So inherit
         from BranchUpdate.
     """
+    def validate_ref_update(self):
+        """See AbstractUpdate.validate_ref_update."""
+        # First, perform the same validation as our parent class
+        # (which means perform the same validation checks as for
+        # branch updates).
+        super(BranchCreation, self).validate_ref_update()
+
+        self.__check_gitreview_defaultbranch()
+
     def get_update_email_contents(self):
         """See AbstractUpdate.get_update_email_contents.
         """
@@ -42,3 +52,81 @@ class BranchCreation(BranchUpdate):
             body += self.summary_of_changes()
 
         return (self.everyone_emails(), subject, body)
+
+    def __check_gitreview_defaultbranch(self):
+        """If .gitreview exists, validate the defaultbranch value.
+
+        This is meant to catch the situation where a user creates
+        a new branch for a repository hosted on gerrit. Those
+        repositories typically have a .gitreview file at their root,
+        providing various information, one of them being the default
+        branch name when sending patches for review. When creating
+        a new branch, it is very easy (and frequent) for a user to
+        forget to also update the .gitreview file, causing patch
+        reviews to be sent with the wrong branch, which later then
+        causes the patch to be merged (submitted) on the wrong
+        branch once it is approved.
+
+        We try to avoid that situation by reading the contents of
+        those files at branch creation time, and reporting an error
+        if it exists and points to a branch name different from ours.
+
+        Note that we only do that for the traditional git branches.
+        We don't worry about the branches in the gerrit-specific
+        special namespaces, for which a user checking out the branch
+        and sending a review is unlikely.
+        """
+        GITREVIEW_FILENAME = '.gitreview'
+        DEFAULTBRANCH_CONFIG_NAME = 'gerrit.defaultbranch'
+
+        # Only perform this check for traditional git branches.
+        # See method description above for the reason why.
+        if not self.ref_name.startswith('refs/heads/'):
+            return
+
+        # If the file doesn't exist for that branch, then there is
+        # no problem.
+        if not file_exists(self.new_rev, GITREVIEW_FILENAME):
+            return
+
+        # Get the contents of the gitreview file, and then get git
+        # to parse its contents. We process it all into a dictionary
+        # rather than just query the value of the one config we are
+        # looking for, for a couple of reasons:
+        #   1. This allows us to avoid having to git returning with
+        #      and error status when the file does not have the config
+        #      entry we are looking for (git returns error code 1
+        #      in that case);
+        #   2. If we even want to look at other configurations in
+        #      that file, the code is already in place to do so.
+
+        gitreview_contents = git.show(
+            '%s:%s' % (self.new_rev, GITREVIEW_FILENAME))
+        gitreview_configs = git.config(
+            '-z', '-l', '--file', '-',
+            _input=gitreview_contents).split('\x00')
+
+        config_map = {}
+        for config in gitreview_configs:
+            if not config:
+                # "git config -z" adds a nul character at the end of
+                # its output, which cause gitreview_configs to end with
+                # an empty entry. Just ignore those.
+                continue
+            config_name, config_val = config.split('\n', 1)
+            config_map[config_name] = config_val
+
+        if DEFAULTBRANCH_CONFIG_NAME in config_map and \
+                config_map[DEFAULTBRANCH_CONFIG_NAME] != self.short_ref_name:
+            raise InvalidUpdate(
+                "Incorrect gerrit default branch name in file `%s'."
+                % GITREVIEW_FILENAME,
+                "You probably forgot to update your %s file following"
+                % GITREVIEW_FILENAME,
+                "the creation of this branch.",
+                '',
+                "Please create a commit which updates the value",
+                "of %s in the file `%s'"
+                % (DEFAULTBRANCH_CONFIG_NAME, GITREVIEW_FILENAME),
+                "and set it to `%s' (instead of `%s')."
+                % (self.short_ref_name, config_map[DEFAULTBRANCH_CONFIG_NAME]))
---
+++ testsuite/tests/P426-022__gitreview_branch_check/test.py
@@ -0,0 +1,133 @@
+from support import *
+
+class TestRun(TestCase):
+    def test_push_forgot(self):
+        """Try pushing the branch named "forgot"
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        # "forgot" is a new branch. In the root directory of that
+        # branch, there is a file named .gitreview, whose default
+        # branch is still "master". Make sure the git-hooks reject
+        # that branch creation.
+        p = Run('git push origin forgot'.split())
+        expected_out = """\
+remote: *** Incorrect gerrit default branch name in file `.gitreview'.
+remote: *** You probably forgot to update your .gitreview file following
+remote: *** the creation of this branch.
+remote: ***
+remote: *** Please create a commit which updates the value
+remote: *** of gerrit.defaultbranch in the file `.gitreview'
+remote: *** and set it to `forgot' (instead of `master').
+remote: error: hook declined to update refs/heads/forgot
+To ../bare/repo.git
+ ! [remote rejected] forgot -> forgot (hook declined)
+error: failed to push some refs to '../bare/repo.git'
+"""
+
+        self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, expected_out)
+
+    def test_push_fixed(self):
+        """Try pushing the branch named "fixed"
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        # "fixed" is a new branch. In the root directory of that
+        # branch, there is a file named .gitreview, whose default
+        # branch is now set to "fixed". So this branch creation
+        # should be accepted.
+        p = Run('git push origin fixed'.split())
+        expected_out = """\
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@adacore.com>
+remote: To: git-hooks-ci@example.com
+remote: Subject: [repo] Created branch 'fixed'
+remote: X-Act-Checkin: repo
+remote: X-Git-Author: Test Suite <testsuite@adacore.com>
+remote: X-Git-Refname: refs/heads/fixed
+remote: X-Git-Oldrev: 0000000000000000000000000000000000000000
+remote: X-Git-Newrev: 8a43f9e571a7ea7422ed0cc4b092700b64eab517
+remote:
+remote: The branch 'fixed' was created pointing to:
+remote:
+remote:  8a43f9e... fix defaultbranch name in .gitreview
+remote: DEBUG: inter-email delay...
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@adacore.com>
+remote: To: git-hooks-ci@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo/fixed] Update a.
+remote: X-Act-Checkin: repo
+remote: X-Git-Author: Joel Brobecker <brobecker@adacore.com>
+remote: X-Git-Refname: refs/heads/fixed
+remote: X-Git-Oldrev: 16d76a092abc9e4a314e5da67d3111f3bb11a56a
+remote: X-Git-Newrev: 863805de6bc5967f9aa2686d1f59c553cf81cf49
+remote:
+remote: commit 863805de6bc5967f9aa2686d1f59c553cf81cf49
+remote: Author: Joel Brobecker <brobecker@adacore.com>
+remote: Date:   Fri Jul 28 14:32:32 2017 -0700
+remote:
+remote:     Update a.
+remote:
+remote: Diff:
+remote: ---
+remote:  a | 1 +
+remote:  1 file changed, 1 insertion(+)
+remote:
+remote: diff --git a/a b/a
+remote: index bf84ca6..22715c0 100644
+remote: --- a/a
+remote: +++ b/a
+remote: @@ -1 +1,2 @@
+remote:  Some file.
+remote: +----------
+remote: DEBUG: inter-email delay...
+remote: DEBUG: Content-Type: text/plain; charset="us-ascii"
+remote: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 7bit
+remote: From: Test Suite <testsuite@adacore.com>
+remote: To: git-hooks-ci@example.com
+remote: Bcc: file-ci@gnat.com
+remote: Subject: [repo/fixed] fix defaultbranch name in .gitreview
+remote: X-Act-Checkin: repo
+remote: X-Git-Author: Joel Brobecker <brobecker@adacore.com>
+remote: X-Git-Refname: refs/heads/fixed
+remote: X-Git-Oldrev: 863805de6bc5967f9aa2686d1f59c553cf81cf49
+remote: X-Git-Newrev: 8a43f9e571a7ea7422ed0cc4b092700b64eab517
+remote:
+remote: commit 8a43f9e571a7ea7422ed0cc4b092700b64eab517
+remote: Author: Joel Brobecker <brobecker@adacore.com>
+remote: Date:   Fri Jul 28 14:33:28 2017 -0700
+remote:
+remote:     fix defaultbranch name in .gitreview
+remote:
+remote: Diff:
+remote: ---
+remote:  .gitreview | 2 +-
+remote:  1 file changed, 1 insertion(+), 1 deletion(-)
+remote:
+remote: diff --git a/.gitreview b/.gitreview
+remote: index 6315846..7939b34 100644
+remote: --- a/.gitreview
+remote: +++ b/.gitreview
+remote: @@ -2,6 +2,6 @@
+remote:  host = git.adacore.com
+remote:  port = 29418
+remote:  project = gdb-testsuite
+remote: -defaultbranch = master
+remote: +defaultbranch = fixed
+remote:  defaultremote = origin
+To ../bare/repo.git
+ * [new branch]      fixed -> fixed
+"""
+
+        self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, expected_out)
+
+if __name__ == '__main__':
+    runtests()

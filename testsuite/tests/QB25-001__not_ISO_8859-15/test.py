93113df97ca77327573d6266773eb3b200b57486
Joel Brobecker <brobecker@adacore.com>
2018-01-08 16:23:26 +0400

    show correct location when identifying non-ISO-8859-15 characters in rev log
    There was a bug when returning an error message indicating that
    the revision contains non-ISO-8859-15, that would always show
    the first line has having the unwanted character, even when
    that characters was actually on a different line.
    
    And while at it, I improved the error message to include the line
    number as well. Just in case showing the text itself is either not
    sufficient or just not as efficient.
    
    Credit for finding the bug: Thomas Quinot (thank you!).
    
    Followup on QB25-001.

--- testsuite/tests/QB25-001__not_ISO_8859-15/test.py
+++ testsuite/tests/QB25-001__not_ISO_8859-15/test.py
@@ -14,7 +14,7 @@ class TestRun(TestCase):
 remote: *** Invalid revision history for commit cee1725c0a285a6e13bbb4536f44f097b6773783:
 remote: *** It contains characters not in the ISO-8859-15 charset.
 remote: ***
-remote: *** Below is the first line where this was detected:
+remote: *** Below is the first line where this was detected (line 1):
 remote: *** | Some revision history with bad chars⁩
 remote: ***                                       ^
 remote: ***                                       |
@@ -24,6 +24,34 @@ remote: *** and try again.
 remote: error: hook declined to update refs/heads/master
 To ../bare/repo.git
  ! [remote rejected] master -> master (hook declined)
+error: failed to push some refs to '../bare/repo.git'
+"""
+
+        self.assertNotEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, expected_out)
+
+    def test_push_bad_after_subject(self):
+        """Try pushing one single-file commit on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        # Push bad-after-subject to the `origin' remote as branch master.
+        # The delta should be one commit with one file being modified.
+        p = Run('git push origin bad-after-subject:master'.split())
+        expected_out = """\
+remote: *** Invalid revision history for commit 48f93aa312e5ae8b64589492730e4ee318368d7f:
+remote: *** It contains characters not in the ISO-8859-15 charset.
+remote: ***
+remote: *** Below is the first line where this was detected (line 3):
+remote: *** | Some revision history with bad chars⁩
+remote: ***                                       ^
+remote: ***                                       |
+remote: ***
+remote: *** Please amend the commit's revision history to remove it
+remote: *** and try again.
+remote: error: hook declined to update refs/heads/master
+To ../bare/repo.git
+ ! [remote rejected] bad-after-subject -> master (hook declined)
 error: failed to push some refs to '../bare/repo.git'
 """
 

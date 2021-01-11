17a6b68842622a18baa45895c85dcbd3b2028013
Joel Brobecker <brobecker@adacore.com>
2021-01-11 12:51:17 +0400

    TC24-018__commit_non_ascii_filename: New testcase
    Change-Id: I66a431b33c3ee6841ea6f974c0ba68db3e60a4c1

---
+++ testsuite/tests/TC24-018__commit_non_ascii_filename/cvs_check.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+"""A dummy cvs_check program that passes all files.
+
+It also prints a trace on stdout, in order to allow us to verify that
+the script was called with the correct arguments for the correct files.
+"""
+from __future__ import print_function
+import sys
+
+# To help with testing, print a trace containing the name of the module
+# and the names of the files being checked.
+print("cvs_check: %s < %s" % (
+    ' '.join(["`%s'" % arg for arg in sys.argv[1:]]),
+    ' '.join(["`%s'" % arg for arg in sys.stdin.read().splitlines(False)])))
---
+++ testsuite/tests/TC24-018__commit_non_ascii_filename/test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from support import *
+
+class TestRun(TestCase):
+    def test_push_commit_on_master(self):
+        """Try pushing one single-file commit on master.
+        """
+        cd ('%s/repo' % TEST_DIR)
+
+        p = Run('git push origin master'.split())
+        expected_out = \
+r"""remote: *** cvs_check: `repo' < `Mânü Scrîpt.txt'
+remote: DEBUG: MIME-Version: 1.0
+remote: Content-Transfer-Encoding: 8bit
+remote: Content-Type: text/plain; charset="utf-8"
+remote: From: Test Suite <testsuite@adacore.com>
+remote: To: git-hooks-ci@example.com
+remote: Bcc: filer@example.com
+remote: Subject: =?utf-8?b?W3JlcG9dIE3Dom7DvCBTY3LDrnB0LnR4dDogTmV3IGZpbGU=?=
+remote: X-Act-Checkin: repo
+remote: X-Git-Author: Joel Brobecker <brobecker@adacore.com>
+remote: X-Git-Refname: refs/heads/master
+remote: X-Git-Oldrev: df3425a769b5509cdac1d9bd0225cfe893f7b688
+remote: X-Git-Newrev: 58067db29f84049f50761f13642a8e4116597276
+remote:
+remote: commit 58067db29f84049f50761f13642a8e4116597276
+remote: Author: Joel Brobecker <brobecker@adacore.com>
+remote: Date:   Thu Dec 24 16:35:52 2020 +0400
+remote:
+remote:     Mânü Scrîpt.txt: New file
+remote:
+remote:     Note: In UTF-8, the following characters are encoded as follow:
+remote:
+remote:       â -> "\303\242" == c3 a2  (latin small letter a with circumflex)
+remote:       ü -> "\303\274" == c3 bc  (latin small letter u with diaeresis)
+remote:       î -> "\303\256" == c3 ae  (latin small letter i with circumflex)
+remote:
+remote: Diff:
+remote: ---
+remote:  "M\303\242n\303\274 Scr\303\256pt.txt" | 1 +
+remote:  1 file changed, 1 insertion(+)
+remote:
+remote: diff --git "a/M\303\242n\303\274 Scr\303\256pt.txt" "b/M\303\242n\303\274 Scr\303\256pt.txt"
+remote: new file mode 100644
+remote: index 0000000..3e71550
+remote: --- /dev/null
+remote: +++ "b/M\303\242n\303\274 Scr\303\256pt.txt"
+remote: @@ -0,0 +1 @@
+remote: +Some text.
+To ../bare/repo.git
+   df3425a..58067db  master -> master
+"""
+
+        self.assertEqual(p.status, 0, p.image)
+        self.assertRunOutputEqual(p, expected_out)
+
+if __name__ == '__main__':
+    runtests()

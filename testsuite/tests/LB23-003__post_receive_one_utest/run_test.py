d31243e6b67eda2cf32b493b1987d8524d98155a
Joel Brobecker <brobecker@adacore.com>
2021-06-27 17:58:25 -0700

    post_receive_one_utest: Stop using testcase.enable_unit_test
    TN: U627-007
    Change-Id: I1ca26a0f6746b2f18b9dc020134b7b6a2e196098

--- testsuite/tests/LB23-003__post_receive_one_utest/run_test.py
+++ testsuite/tests/LB23-003__post_receive_one_utest/run_test.py
@@ -1,35 +1,16 @@
 from support import *
-from StringIO import StringIO
-import sys
 
 
 class TestRun(TestCase):
     def test_post_receive_one(testcase):
         """Unit test utils.get_user_name."""
-        testcase.enable_unit_test()
-
-        # Redirect stdout in order to capture it, and check its contents.
-        old_stderr = sys.stderr
-        sys.stderr = StringIO()
-
-        from post_receive import post_receive_one
-
-        post_receive_one(
-            "refs/heads/v1",
-            "0000000000000000000000000000000000000000",
-            "30242ad9fa3091c81e55a7b1349ab83f8c1b04e7",
-            None,
-            None,
-        )
-        expected_out = """\
+        testcase.run_unit_test_script(
+            expected_out="""\
 *** post-receive: Unsupported reference update: refs/heads/v1 (ignored).
 ***               old_rev = 0000000000000000000000000000000000000000
 ***               new_rev = 30242ad9fa3091c81e55a7b1349ab83f8c1b04e7
 """
-        testcase.assertEqual(sys.stderr.getvalue(), expected_out)
-
-        # Restore sys.stderr...
-        sys.stderr = old_stderr
+        )
 
 
 if __name__ == "__main__":
---
+++ testsuite/tests/LB23-003__post_receive_one_utest/unit_test_script.py
@@ -0,0 +1,9 @@
+from post_receive import post_receive_one
+
+post_receive_one(
+    "refs/heads/v1",
+    "0000000000000000000000000000000000000000",
+    "30242ad9fa3091c81e55a7b1349ab83f8c1b04e7",
+    None,
+    None,
+)

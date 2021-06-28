941cd5129779807c47f36d0f1c293412f92d216f
Joel Brobecker <brobecker@adacore.com>
2021-06-27 18:07:47 -0700

    type_conversions_utest: Stop using testcase.enable_unit_test
    TN: U627-007
    Change-Id: I5f1abbf3ce494ed905c00d5a7ffd0dd7abf03334

--- testsuite/tests/LC28-003__type_conversions_utest/run_test.py
+++ testsuite/tests/LC28-003__type_conversions_utest/run_test.py
@@ -4,15 +4,12 @@ from support import *
 class TestRun(TestCase):
     def test_get_user_name(testcase):
         """Unit test utils.get_user_name."""
-        testcase.enable_unit_test()
-
-        from type_conversions import to_bool
-
-        # The purpose of this test is to test that do coverage
-        # testing of the case where to_bool is called with
-        # a boolean value.
-        testcase.assertEqual(to_bool(True), True)
-        testcase.assertEqual(to_bool(False), False)
+        testcase.run_unit_test_script(
+            expected_out="""\
+True
+False
+"""
+        )
 
 
 if __name__ == "__main__":
---
+++ testsuite/tests/LC28-003__type_conversions_utest/unit_test_script.py
@@ -0,0 +1,7 @@
+from type_conversions import to_bool
+
+# The purpose of this test is to test that do coverage
+# testing of the case where to_bool is called with
+# a boolean value.
+print(to_bool(True))
+print(to_bool(False))

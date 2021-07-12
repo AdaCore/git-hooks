179346fafbf5ae9a788a490ae75e11ed0c9368a4
Joel Brobecker <brobecker@adacore.com>
2021-07-11 17:51:32 -0700

    cannot_find_style_checker/run_test.py: Use "format" method instead of "%"
    This commit slightly rewrites the way we format the expected output
    of our of our tests, by using the "format" method instead of using
    the "%" operator. It doesn't make much of a difference, at the moment,
    but this will become handy when we transition to Python 3.x, where
    the "No such file or directory" error now includes the path of
    the file/directory that's missing.
    
    TN: U530-006
    Change-Id: I748ca76d411cfdf79dd33402d1297e7372decc38

--- testsuite/tests/Q530-009__cannot_find_style_checker/run_test.py
+++ testsuite/tests/Q530-009__cannot_find_style_checker/run_test.py
@@ -1,4 +1,5 @@
 from support import *
+import os
 
 
 class TestRun(TestCase):
@@ -7,16 +8,15 @@ class TestRun(TestCase):
         # Push master to the `origin' remote.  The delta should be one
         # commit with one file being modified.
         p = testcase.run("git push origin master".split())
-        expected_out = (
-            """\
-remote: *** Invalid hooks.style-checker configuration (%s/cvs_check.py):
+        expected_out = """\
+remote: *** Invalid hooks.style-checker configuration ({cvs_check_script}):
 remote: [Errno 2] No such file or directory
 remote: error: hook declined to update refs/heads/master
 To ../bare/repo.git
  ! [remote rejected] master -> master (hook declined)
 error: failed to push some refs to '../bare/repo.git'
-"""
-            % testcase.work_dir
+""".format(
+            cvs_check_script=os.path.join(testcase.work_dir, "cvs_check.py")
         )
 
         testcase.assertNotEqual(p.status, 0, p.image)
